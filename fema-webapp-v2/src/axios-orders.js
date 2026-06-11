352f2d9008727be6a753a8ef95197d5856b7e2b6
Robert Munoz <52767814+rmunoz2165@users.noreply.github.com>
2026-06-11 12:50:08 +0100

    Delete fema-webapp-v2 directory
    deleted due to bug issues

--- fema-webapp-v2/src/axios-orders.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://fema-damage-report.firebaseio.com/'
-})
-
-
-export default instance;
--- fema-webapp-v2/src/components/Layout/UI/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Spinner.css'
-
-const Spinner = (props) => (
-    <div className={classes.loader}></div>
-    );
-
-export default Spinner;
